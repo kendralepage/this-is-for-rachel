bae08d1f8fe11dc0b2ebf8129d6e9b8c98accd51
kendralepage <56355042+kendralepage@users.noreply.github.com>
2020-04-12 15:44:08 -0400

    Update fullTextManualInput.py

--- fullTextManualInput.py
+++ fullTextManualInput.py
@@ -6,13 +6,14 @@ Send full text with number input
 
 '''
 import os
-from time import sleep
+
 from subprocess import Popen, PIPE
 def send_Message():
-    getFile=input(str("Enter file name: "))
-    infile=open(getFile, 'r')
-    number=input(str("Enter phone number: "))
-    words=infile.read()
+    getFile=input(str("Enter file name: ")) #get file name
+    infile=open(getFile, 'r') #open file
+    number=input(str("Enter phone number: "))#get phone number
+    words=infile.read()#set message
+    #start applescript
     scpt = '''on run {targetBuddyPhone, targetMessage}
             tell application "Messages"
             set targetService to 1st service whose service type = iMessage
@@ -20,11 +21,12 @@ def send_Message():
             send targetMessage to targetBuddy
             end tell
             end run'''
-    args = [number, words]
+    #end applescript
+    args = [number, words] #set arguments
     p = Popen(['osascript', '-'] + args, stdin=PIPE, stdout=PIPE, stderr=PIPE, universal_newlines=True)
-    stdout, stderr = p.communicate(scpt)
+    stdout, stderr = p.communicate(scpt) #pass arguments
            
 
            
 
-send_Message()
+send_Message() #full send
